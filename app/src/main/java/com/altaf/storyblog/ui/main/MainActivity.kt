a2ceb9dc1035f1dd934aecc3e68cf142ed7cf816
Altaf <altaf549@gmail.com>
2025-10-22 14:42:39 +0530

    Update MainActivity, add bottom navigation theming and custom toolbar

--- app/src/main/java/com/altaf/storyblog/ui/main/MainActivity.kt
+++ app/src/main/java/com/altaf/storyblog/ui/main/MainActivity.kt
@@ -20,12 +20,47 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     override fun setupUI() {
         enableEdgeToEdge()
+        setupToolbar()
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
         
         // Initialize NavController after the view is created
+        setupNavigation()
+    }
+    
+    private fun setupToolbar() {
+        binding.toolbar.toolbar.let { toolbar ->
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayShowTitleEnabled(false) // Disable default title
+            
+            // Set status bar color to match toolbar
+            window.statusBarColor = getColor(R.color.colorActionBar)
+            
+            // Set light status bar for better visibility of icons
+            window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or 
+                android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+    
+    private fun setupNavigation() {
         binding.main.post {
             val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
             val navController = navHostFragment?.navController ?: return@post
             binding.bottomNav.setupWithNavController(navController)
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                binding.toolbar.toolbarTitle.text = destination.label
+                /*when (destination.id) {
+                    R.id.homeFragment -> updateToolbarTitle("Home")
+                    R.id.categoryFragment -> updateToolbarTitle("Search")
+                    R.id.storyFragment -> updateToolbarTitle("Add Post")
+                    R.id.settingFragment -> updateToolbarTitle("Notifications")
+                    R.id.profileFragment -> updateToolbarTitle("Profile")
+                }*/
+            }
         }
     }
 }
